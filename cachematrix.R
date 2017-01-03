5454f15c3f1882588989962083ec6d38c56f6e8e
jarrenLS <jarren.santos.js@gmail.com>
2017-01-03 00:03:50 -0800

    Editing the layout for my liking

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall Description: ##
 
-## Write a short comment describing this function
+
+
+## makeCacheMatrix ##
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve ##
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
